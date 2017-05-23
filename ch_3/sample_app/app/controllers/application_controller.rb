39f065c3955aeb93aeec3b5bf88d4d5f04dde334
SubSeven57 <danehernandez32@gmail.com>
2017-05-22 19:21:23 -0500

    Added Hello page for debugging

--- ch_3/sample_app/app/controllers/application_controller.rb
+++ ch_3/sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello again, this is the third time I do this."
+  end  
 end
