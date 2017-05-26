58a998f20f651f644360f484b44fef3656fc1836
SubSeven57 <danehernandez32@gmail.com>
2017-05-26 17:08:34 -0500

    Modified routes to display my Home page and deleted sample Hello function

--- ch_3/sample_app/app/controllers/application_controller.rb
+++ ch_3/sample_app/app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "Hello again, this is the third time I do this."
-  end  
 end
